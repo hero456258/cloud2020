d2352d8b306b14f7fdfe38a4d8ccde417db8af79
wanggang <mq316422735>
2020-04-03 21:30:10 +0800

    支付服务提供者8001入驻进EurekaServer

--- cloud-provider-payment8001/src/main/java/com/mingqian/springcloud/PaymentMain8001.java
+++ cloud-provider-payment8001/src/main/java/com/mingqian/springcloud/PaymentMain8001.java
@@ -2,12 +2,14 @@ package com.mingqian.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * @Author: mingqian
  * @DATE: 2020/4/3 11:33
  */
 @SpringBootApplication
+@EnableEurekaClient
 public class PaymentMain8001 {
 
     public static void main(String[] args) {
