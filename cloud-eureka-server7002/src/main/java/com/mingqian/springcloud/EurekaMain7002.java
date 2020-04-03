b340602122ebaf8fb05459b9c7deb425062e7d7f
wanggang <mq316422735>
2020-04-03 22:08:01 +0800

    eurekaServer集群：7001和7002互相注册，相互守望

---
+++ cloud-eureka-server7002/src/main/java/com/mingqian/springcloud/EurekaMain7002.java
@@ -0,0 +1,19 @@
+package com.mingqian.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @Author: mingqian
+ * @DATE: 2020/4/3 11:33
+ * @Desc: Eureka服务端
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaMain7002 {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaMain7002.class, args);
+    }
+}
