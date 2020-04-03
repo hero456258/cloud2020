39d0ca12c46c1460cba92e02e5994355968864fc
wanggang <mq316422735>
2020-04-03 21:42:43 +0800

    支付服务消费者80入驻进EurekaServer

--- cloud-consumer-order80/src/main/java/com/mingqian/springcloud/OrderMain80.java
+++ cloud-consumer-order80/src/main/java/com/mingqian/springcloud/OrderMain80.java
@@ -2,12 +2,14 @@ package com.mingqian.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * @Author: mingqian
  * @DATE: 2020/4/3 15:36
  */
 @SpringBootApplication
+@EnableEurekaClient
 public class OrderMain80 {
     public static void main(String[] args) {
         SpringApplication.run(OrderMain80.class, args);
