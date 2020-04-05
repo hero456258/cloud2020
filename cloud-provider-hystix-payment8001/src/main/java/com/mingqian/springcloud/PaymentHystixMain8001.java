8e6228994dc4ca4c148da6fbc1881c666c9e49c8
wanggang <mq316422735>
2020-04-05 10:33:35 +0800

    cloud-provider-hystix-payment8001服务降级处理，异常超时都是一个处理结果

--- cloud-provider-hystix-payment8001/src/main/java/com/mingqian/springcloud/PaymentHystixMain8001.java
+++ cloud-provider-hystix-payment8001/src/main/java/com/mingqian/springcloud/PaymentHystixMain8001.java
@@ -2,8 +2,8 @@ package com.mingqian.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 
 /**
  * @Author: mingqian
@@ -11,7 +11,7 @@ import org.springframework.cloud.netflix.hystrix.EnableHystrix;
  */
 @SpringBootApplication
 @EnableEurekaClient
-//@EnableHystrix
+@EnableCircuitBreaker
 public class PaymentHystixMain8001 {
     public static void main(String[] args) {
         SpringApplication.run(PaymentHystixMain8001.class, args);
--- cloud-provider-hystix-payment8001/src/main/java/com/mingqian/springcloud/service/impl/PaymentServiceImpl.java
+++ cloud-provider-hystix-payment8001/src/main/java/com/mingqian/springcloud/service/impl/PaymentServiceImpl.java
@@ -1,6 +1,8 @@
 package com.mingqian.springcloud.service.impl;
 
 import com.mingqian.springcloud.service.PaymentService;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
 import org.springframework.stereotype.Service;
 
 import java.util.concurrent.TimeUnit;
@@ -14,26 +16,37 @@ public class PaymentServiceImpl implements PaymentService {
 
     /**
      * 正常访问的方法
+     *
      * @param id
      * @return
      */
     @Override
     public String paymentInfoOk(Integer id) {
 
-        return "线程池 " + Thread.currentThread().getName() + "  paymentInfoOk,id: " + id + "\t" + "哈哈" ;
+        return "线程池 " + Thread.currentThread().getName() + "  paymentInfoOk,id: " + id + "\t" + "哈哈";
     }
 
+    @HystrixCommand(fallbackMethod = "paymentInfoTimeOutHandler", commandProperties = {
+            @HystrixProperty(name = "execution.isolation.thread.timeoutInMilliseconds", value = "3000")
+    })
     @Override
     public String paymentInfoTimeOut(Integer id) {
 
-        int sleepNumber = 3;
-        try {
-            TimeUnit.SECONDS.sleep(sleepNumber);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
+//        int sleepNumber = 5;\n" +
+//                "        try {\n" +
+//                "            TimeUnit.SECONDS.sleep(sleepNumber);\n" +
+//                "        } catch (InterruptedException e) {\n" +
+//                "            e.printStackTrace();\n" +
+//                "        }\n" +
+//                "\n" +
+//                "        return \"线程池 \" + Thread.currentThread().getName() + \"  paymentInfoTimeOut,id: \" + id + \"\t" + "哈哈" + "耗时"+ sleepNumber +"秒钟" ;
+        int age = 10 / 0;
+        return "线程池 " + Thread.currentThread().getName() + "  paymentInfoTimeOut,id:" + id;
 
-        return "线程池 " + Thread.currentThread().getName() + "  paymentInfoTimeOut,id: " + id + "\t" + "哈哈" + "耗时"+ sleepNumber +"秒钟" ;
+    }
+
+    public String paymentInfoTimeOutHandler(Integer id) {
+        return "线程池 " + Thread.currentThread().getName() + "  paymentInfoTimeOutHandler,id: " + id + "\t" + "smile  ";
 
     }
 }
