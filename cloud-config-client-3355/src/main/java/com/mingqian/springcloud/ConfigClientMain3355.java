820e7b6a884722fc9a37f48da413685fb3259e7e
wanggang <mq316422735>
2020-04-06 16:10:50 +0800

    cloud-config-client-3355创建配置中心客户端并成功链接到github

---
+++ cloud-config-client-3355/src/main/java/com/mingqian/springcloud/ConfigClientMain3355.java
@@ -0,0 +1,17 @@
+package com.mingqian.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * @Author: mingqian
+ * @DATE: 2020/4/6 15:48
+ */
+@SpringBootApplication
+@EnableEurekaClient
+public class ConfigClientMain3355 {
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigClientMain3355.class, args);
+    }
+}
---
+++ cloud-config-client-3355/src/main/java/com/mingqian/springcloud/controller/ConfigClientController.java
@@ -0,0 +1,21 @@
+package com.mingqian.springcloud.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @Author: mingqian
+ * @DATE: 2020/4/6 15:52
+ */
+@RestController
+public class ConfigClientController {
+
+    @Value("${config.info}")
+    private String configInfo;
+
+    @GetMapping("/configInfo")
+    public String getConfigInfo(){
+        return configInfo;
+    }
+}
