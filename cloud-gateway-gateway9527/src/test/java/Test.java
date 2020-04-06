55cc37de1d16b2bd266bcdabffe2dac49babf456
wanggang <mq316422735>
2020-04-06 11:17:35 +0800

    cloud-gateway-gateway9527网关中predicate使用

---
+++ cloud-gateway-gateway9527/src/test/java/Test.java
@@ -0,0 +1,12 @@
+import java.time.ZonedDateTime;
+
+/**
+ * @Author: mingqian
+ * @DATE: 2020/4/6 10:41
+ */
+public class Test {
+    public static void main(String[] args) {
+        ZonedDateTime now = ZonedDateTime.now();
+        System.out.println(now); //2020-04-06T10:42:29.070073300+08:00[Asia/Shanghai]
+    }
+}
