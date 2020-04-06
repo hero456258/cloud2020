d57098240a44b4114795708d1e40af51469025cd
wanggang <mq316422735>
2020-04-06 09:56:22 +0800

    cloud-gateway-gateway9527添加网关

---
+++ cloud-gateway-gateway9527/src/main/java/com/mingqian/springcloud/GateWayMain9527.java
@@ -0,0 +1,17 @@
+package com.mingqian.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * @Author: mingqian
+ * @DATE: 2020/4/6 9:42
+ */
+@SpringBootApplication
+@EnableEurekaClient
+public class GateWayMain9527 {
+    public static void main(String[] args) {
+        SpringApplication.run(GateWayMain9527.class, args);
+    }
+}
