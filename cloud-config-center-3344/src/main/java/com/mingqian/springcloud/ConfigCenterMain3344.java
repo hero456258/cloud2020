9501fdb6a144664170423a2b2ce20245011aa184
wanggang <mq316422735>
2020-04-06 15:31:36 +0800

    cloud-config-center-3344创建配置中心并成功链接到github

---
+++ cloud-config-center-3344/src/main/java/com/mingqian/springcloud/ConfigCenterMain3344.java
@@ -0,0 +1,17 @@
+package com.mingqian.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+/**
+ * @Author: mingqian
+ * @DATE: 2020/4/6 15:15
+ */
+@SpringBootApplication
+@EnableConfigServer
+public class ConfigCenterMain3344 {
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigCenterMain3344.class, args);
+    }
+}
