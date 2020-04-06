8acd966885241d0b099c8ce55e8555f3283901af
wanggang <mq316422735>
2020-04-06 11:30:41 +0800

    cloud-gateway-gateway9527网关使用全局GlobalFilter(自定义全局Filter)

---
+++ cloud-gateway-gateway9527/src/main/java/com/mingqian/springcloud/filter/MyLogGateWayFilter.java
@@ -0,0 +1,41 @@
+package com.mingqian.springcloud.filter;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.core.Ordered;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+import java.time.LocalDateTime;
+
+/**
+ * @Author: mingqian
+ * @DATE: 2020/4/6 11:18
+ */
+@Component
+@Slf4j
+public class MyLogGateWayFilter implements GlobalFilter, Ordered {
+    @Override
+    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+
+        log.info("*************come in MyLogGateWayFilter***************" + LocalDateTime.now());
+
+        String username = exchange.getRequest().getQueryParams().getFirst("username");
+        if (StringUtils.isEmpty(username)){
+            log.info("用户名为空，非法用户");
+            exchange.getResponse().setStatusCode(HttpStatus.NOT_ACCEPTABLE);
+            return exchange.getResponse().setComplete();
+        }
+
+        return chain.filter(exchange);
+    }
+
+    @Override
+    public int getOrder() {
+        return 0;
+    }
+}
