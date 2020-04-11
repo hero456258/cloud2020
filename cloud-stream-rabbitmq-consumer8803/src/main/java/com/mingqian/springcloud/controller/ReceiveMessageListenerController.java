ae3e80f48dfcc847811001f648aa11311d06160d
wanggang <mq316422735>
2020-04-11 18:17:59 +0800

    cloud-stream-rabbitmq-consumer8803/8802 cloudStream消息消费者通过分组(group)解决重复消费和消息持久化问题

---
+++ cloud-stream-rabbitmq-consumer8803/src/main/java/com/mingqian/springcloud/controller/ReceiveMessageListenerController.java
@@ -0,0 +1,23 @@
+package com.mingqian.springcloud.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.stream.annotation.EnableBinding;
+import org.springframework.cloud.stream.annotation.StreamListener;
+import org.springframework.cloud.stream.messaging.Sink;
+import org.springframework.messaging.Message;
+import org.springframework.web.bind.annotation.RestController;
+
+@EnableBinding(Sink.class)
+@RestController
+public class ReceiveMessageListenerController
+{
+    @Value("${server.port}")
+    private String serverPort;
+
+
+    @StreamListener(Sink.INPUT)
+    public void input(Message<String> message)
+    {
+        System.out.println("消费者2号,----->接受到的消息: "+message.getPayload()+"\t  port: "+serverPort);
+    }
+}
